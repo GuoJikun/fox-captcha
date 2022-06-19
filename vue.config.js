fb9c06bf9a62d588b245c22d42a10275730649d5
GuoJikun <guojikun@y-open.com>
2022-06-19 11:08:24 +0800

    feat: 优化

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-    publicPath: "/captcha/",
+    publicPath: "/fox-captcha/",
     transpileDependencies: true,
 });
